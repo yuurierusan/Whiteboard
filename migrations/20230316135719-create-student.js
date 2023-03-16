78fb86c4da8ae53150c1f4f46c3bff556bede11c
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-03-16 11:45:38 -0600

    Made changes to grades migrations

--- migrations/20230316135719-create-student.js
+++ migrations/20230316135719-create-student.js
@@ -1,41 +1,41 @@
 'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('students', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER,
-            },
-            name: { type: DataTypes.STRING, allowNull: false },
-            email: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true,
-                validate: {
-                    isEmail: true,
-                },
-            },
-            course: {
-                type: Sequelize.STRING,
-            },
-            passwordDigest: {
-                type: Sequelize.STRING,
-                allowNull: false,
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-            },
-        })
-    },
-    async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('students')
-    },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: { type: Sequelize.STRING, allowNull: false },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      course: {
+        type: Sequelize.STRING
+      },
+      passwordDigest: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('students')
+  }
 }
