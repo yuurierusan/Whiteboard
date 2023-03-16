c2efece545c6f4a95a4557f5f9e4bca2372948ca
Jonshea Nutson <yuurieru@icloud.com>
2023-03-16 19:16:00 -0400

    moved login page and nav bar

---
+++ client/src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="form-container">
+        <form v-on:submit="handleSubmit">
+            <input
+                @input="handleChange"
+                placeholder="Email"
+                :value="email"
+                name="email"
+                type="email" />
+            <input
+                @input="handleChange"
+                placeholder="Password"
+                :value="password"
+                name="password"
+                type="password" />
+            <button :disabled="!email || !password">Log In</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginPage',
+    props: {
+        email: String,
+        password: String,
+    },
+    methods: {
+        handleChange() {},
+        handleSubmit() {},
+    },
+}
+</script>
