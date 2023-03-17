601f99b34bcd69d00720427f55c1e428d89a4aee
Jonshea Nutson <yuurieru@icloud.com>
2023-03-17 01:22:26 -0400

    able to map course on courses page & added search

---
+++ client/src/components/CourseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="courses-card"
+        ><h2>{{ course.name }}</h2>
+        <p>{{ course.grade }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CourseCard',
+    props: {
+        course: {},
+    },
+    methods: {
+        getCourse(id) {
+            this.$emit('getCourse', id)
+        },
+    },
+}
+</script>
