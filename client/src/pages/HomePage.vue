7d268d6766bf8cc72238adefee8e64e222162b0b
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-03-16 23:30:28 -0600

    new pages created

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,26 +1,35 @@
 <template>
     <div id="app">
         <header> </header>
-        <img alt="Whiteboard logo" src="../assets/logo.jpeg" />
-        <Login
-            :email="email"
-            :password="password"
-            @handleFormChange="handleFormChange"
-            @handleSubmit="handleSubmit" />
-    </div>
+        <div v-if="!logedIn">
+          <img alt="Whiteboard logo" src="../assets/logo.jpeg" />
+          <Login
+          :email="email"
+          :password="password"
+          @handleFormChange="handleFormChange"
+          @handleSubmit="handleSubmit" />
+        </div>
+        <div v-if="logedIn">
+          <CoursesPage />
+
+        </div>
+      </div>
 </template>
 
 <script>
 import Login from '../components/Login.vue'
-
+import CoursesPage from './CoursesPage.vue'
 export default {
     name: 'HomePage',
     components: {
         Login,
+        CoursesPage
     },
     data: () => ({
         email: '',
         password: '',
+        logedIn: false
+
     }),
     mounted: function () {},
     methods: {
@@ -30,6 +39,7 @@ export default {
         handleSubmit(name, value) {
             this[name] = ''
             this[value] = ''
+            this.logedIn = true
         },
     },
 }
