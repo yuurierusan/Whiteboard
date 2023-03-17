a50e26c23219432de0b7402bc7814ba2ea6970f0
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-16 22:29:50 -0700

    build AddStudent

--- client/src/components/AddStudent.vue
+++ client/src/components/AddStudent.vue
@@ -1,1 +1,50 @@
-<template> </template>
+<template>
+  <div class="addstudent">
+    <h1>Add a Student</h1>
+    <div class="studentform">
+      <form @submit.prevent="addStudent">
+        <div>
+          <label for="name">Name:</label>
+          <input id="name" type="text" v-model="name" />
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input id="email" type="email" v-model="email" />
+        </div>
+        <button type="submit">Add Student</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'AddStudent',
+    data: () => ({
+      name: '',
+      email: ''
+    }),
+    methods: {
+      async createStudent() {
+        const newStudent = {
+          name: this.name,
+          email: this.email
+        };
+        try {
+          const response = await axios.post('http://localhost:3001/api/students/create', newStudent)
+          console.log(response.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .addstudent {
+    color: yellow;
+  }
+</style>
--- client/src/router.js
+++ client/src/router.js
@@ -5,43 +5,49 @@ import CourseDetails from './pages/CourseDetails'
 import StudentDetails from './pages/StudentDetails'
 import CoursesPage from './pages/CoursesPage'
 import StudentsPage from './pages/StudentsPage'
+import AddStudent from './components/AddStudent'
 
 const routes = [
-    {
-        path: '/',
-        component: HomePage,
-        name: 'HomePage',
-    },
-    {
-        path: '/about',
-        component: AboutPage,
-        name: 'AboutPage',
-    },
-    {
-        path: '/courses',
-        component: CoursesPage,
-        name: 'CoursesPage',
-    },
-    {
-        path: '/students',
-        component: StudentsPage,
-        name: 'StudentsPage',
-    },
-    {
-        path: '/:id',
-        component: CourseDetails,
-        name: 'CourseDetails',
-    },
-    {
-        path: '/:studentId',
-        component: StudentDetails,
-        name: 'StudentDetails',
-    },
+  {
+    path: '/',
+    component: HomePage,
+    name: 'HomePage'
+  },
+  {
+    path: '/about',
+    component: AboutPage,
+    name: 'AboutPage'
+  },
+  {
+    path: '/courses',
+    component: CoursesPage,
+    name: 'CoursesPage'
+  },
+  {
+    path: '/students',
+    component: StudentsPage,
+    name: 'StudentsPage'
+  },
+  {
+    path: '/:id',
+    component: CourseDetails,
+    name: 'CourseDetails'
+  },
+  {
+    path: '/:studentId',
+    component: StudentDetails,
+    name: 'StudentDetails'
+  },
+  {
+    path: '/addStudent',
+    component: AddStudent,
+    name: 'AddStudent'
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes
 })
 
 export default router
